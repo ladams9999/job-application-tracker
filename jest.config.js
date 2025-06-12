c23581ea85ab7560048d5d04660fe3663e8faa71
Lloyd <ladams9999@users.noreply.github.com>
2025-06-11 23:24:40 -0700

    Fix Jest config for ESM and add tsconfig for tests

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,20 @@
 
-module.exports = {
+export default {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  moduleNameMapping: {
+  moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+      tsconfig: './tsconfig.jest.json',
+    },
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   testMatch: [
